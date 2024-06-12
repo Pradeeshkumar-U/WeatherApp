9d4d6c441782a7de37ec486c448e42ce2541f77c
Pradeeshkumar <pradeeshkumar6382@gmail.com>
2024-06-12 20:43:22 +0530

    new edit

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:weather_app/utilities/constants.dart';
 
 class LocationScreen extends StatefulWidget {
+  const LocationScreen({super.key});
+
   @override
+  // ignore: library_private_types_in_public_api
   _LocationScreenState createState() => _LocationScreenState();
 }
 
