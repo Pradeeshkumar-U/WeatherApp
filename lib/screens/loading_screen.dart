2fe250a6634a132444c13a09fe14f60464f954a2
Pradeeshkumar <pradeeshkumar6382@gmail.com>
2024-06-15 14:25:07 +0530

    new edit

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
@@ -11,21 +13,59 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class LoadingScreenState extends State<LoadingScreen> {
-  void geoLocator() async {
-    Position positionOf = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
-    print(positionOf);
+  String position = '';
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  Future getLocation() async {
+    try {
+      bool check;
+      LocationPermission permission;
+      check = await Geolocator.isLocationServiceEnabled();
+      if (!check) {
+        permission = await Geolocator.requestPermission();
+        setState(() {
+          position = ('ENABLE LOCATION SERVICES');
+        });
+      }
+      permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          setState(() {
+            position = ('PERMISSION DENIED');
+          });
+        }
+      }
+      if (permission == LocationPermission.deniedForever) {
+        setState(() {
+          position = ('PERMISSION DENIED FOREVER');
+        });
+      }
+
+      Position location = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      setState(() {
+        position = 'lati:${location.latitude}\tlong:${location.longitude}';
+      });
+    } catch (e) {
+      setState(() {
+        position = 'Unable to get position ($e)';
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            geoLocator();
-          },
-          child: const Text('Get Location'),
+        child: Text(
+          position,
+          textAlign: TextAlign.center,
+          style: const TextStyle(fontSize: 20, color: Colors.white),
         ),
       ),
     );
